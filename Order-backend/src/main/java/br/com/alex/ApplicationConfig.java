b612c0e3234aaa7a737575fd59599e0cca583f88
AlexandruLucian <alexandru.lucian89@gmail.com>
2017-11-09 14:32:32 +0000

    First commit

---
+++ Order-backend/src/main/java/br/com/alex/ApplicationConfig.java
@@ -0,0 +1,25 @@
+package br.com.alex;
+
+import javax.inject.Named;
+
+import org.glassfish.jersey.server.ResourceConfig;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class ApplicationConfig {
+	
+	@Named
+	static class JerseyConfig extends ResourceConfig {
+		public JerseyConfig() {
+			this.packages("br.com.alex.rest");
+		}
+	}
+	
+	@Bean
+	public RestTemplate restTemplate() {
+		RestTemplate restTemplate = new RestTemplate();
+		return restTemplate;
+	}
+}
